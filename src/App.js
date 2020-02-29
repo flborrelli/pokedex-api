08f3b895cd5d3a3669cb66d796efb80ea0599a34
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-02-28 21:49:42 -0300

    Fetching pokemon additional infos working, remaining update states

--- src/App.js
+++ src/App.js
@@ -6,22 +6,38 @@ import axios from 'axios';
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
+  const [pokemonInfos, setPokemonInfos] = useState({
+    id: '',
+    avatar: ''
+  })
+
 
   useEffect(() => {
     getAPIData()
   }, []);
 
 
-  const rootAPI_URL = 'https://pokeapi.co/api/v2/pokemon';
-
   const getAPIData = () => {
+    const rootAPI_URL = 'https://pokeapi.co/api/v2/pokemon?limit=20';
     axios.get(rootAPI_URL)
     .then(responseFromAPI => {
       const pokemonArray = responseFromAPI.data.results;
       console.log(pokemonArray)
+      pokemonArray.map(pokemon => {
+        getPokemonInfos(pokemon)
+      })
       setPokemons(pokemonArray.map(e => e.name))
     })
-    .catch(error => console.log('An error occured:', error))
+    .catch(error => console.log('An error occured while fetching API:', error))
+  }
+
+  const getPokemonInfos = (pokemon) => {
+    const url = pokemon.url;
+    axios.get(url)
+    .then(pokemonInfos => {
+      console.log(pokemonInfos)
+    })
+    .catch(error => error => console.log('An error occured while accessing Pokemon Infos:', error))
   }
 
   return (
