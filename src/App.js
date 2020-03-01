dee399e82696484e26a5420a54ce77f701aab011
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-02-29 21:24:53 -0300

    Implementing async await instead of then catch in order to access multiple api endpoints

--- src/App.js
+++ src/App.js
@@ -5,46 +5,61 @@ import PokemonList from './components/PokemonList';
 import axios from 'axios';
 
 function App() {
-  const [pokemons, setPokemons] = useState([]);
-  const [pokemonInfos, setPokemonInfos] = useState({
-    id: '',
-    avatar: ''
-  })
-
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     getAPIData()
   }, []);
 
+  const getAPIData = async () => {
+    try{
+      const responseFromAPI = await getAllPokemonData('https://pokeapi.co/api/v2/pokemon?limit=20');
+      console.log(responseFromAPI)
+    }
+    catch(error){
+      console.log('An error occured while fetching data from API', error)
+    }
+  }
 
-  const getAPIData = () => {
-    const rootAPI_URL = 'https://pokeapi.co/api/v2/pokemon?limit=20';
-    axios.get(rootAPI_URL)
-    .then(responseFromAPI => {
-      const pokemonArray = responseFromAPI.data.results;
-      console.log(pokemonArray)
-      pokemonArray.map(pokemon => {
-        getPokemonInfos(pokemon)
+  //Fetching API data
+  const getAllPokemonData = async (url) => {
+    return new Promise((resolve, reject) => {
+      axios.get(url)
+      .then(data => {
+        resolve(data);
       })
-      setPokemons(pokemonArray.map(e => e.name))
     })
-    .catch(error => console.log('An error occured while fetching API:', error))
   }
 
-  const getPokemonInfos = (pokemon) => {
-    const url = pokemon.url;
-    axios.get(url)
-    .then(pokemonInfos => {
-      console.log(pokemonInfos)
-    })
-    .catch(error => error => console.log('An error occured while accessing Pokemon Infos:', error))
-  }
 
+
+  // const getAPIData = () => {
+  //   const rootAPI_URL = 'https://pokeapi.co/api/v2/pokemon?limit=20';
+  //   axios.get(rootAPI_URL)
+  //   .then(responseFromAPI => {
+  //     const pokemonArray = responseFromAPI.data.results;
+  //     pokemonArray.map(pokemon => {
+  //       return getPokemonInfos(pokemon)
+  //     })
+  //     setPokemons(pokemonArray.map(e => e.name))
+  //   })
+  //   .catch(error => console.log('An error occured while fetching API:', error))
+  // }
+
+  // const getPokemonInfos = (pokemon) => {
+  //   const url = pokemon.url;
+  //   axios.get(url)
+  //   .then(pokeInfos => {
+  //     })
+  //   .catch(error => error => console.log('An error occured while accessing Pokemon Infos:', error))
+  // }
   return (
     <div>
       <NavBar />
-      <PokemonList getPokemons={pokemons}/>
-      {/* {pokemons.map(e => e)} */}
+      <div>
+      
+      </div>
+      <PokemonList getPokemon={pokemonData}/>
     </div>
   );
 }
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Segment } from 'semantic-ui-react'
 
-function PokemonList( { getPokemons } ) {
+function PokemonList( { getPokemon } ) {
   return (
     <div className='d-flex justify-content-center mt-5'>
-      <div className='pokelist'>
-        {getPokemons.map(e => {
+      {/* <div className='pokelist'>
+        {getPokemon.map(e => {
           return <Segment raised key={e}>{e}</Segment>
           })}
-      </div>
+      </div> */}
+
+      {/* <div className='pokelist'>
+        {getId.map(e => {
+          return <Segment raised key={e}>{e}</Segment>
+          })}
+      </div> */}
       
     </div>
   );
